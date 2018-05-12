e16778f81aea10ccfbe75a08f97eba75312604dc
AnusaraTrokhum <anusara.orm@gmail.com>
2018-05-12 12:16:29 +0700

    Initial commit

---
+++ app/src/main/java/com/example/anusaratrokhum/advenger_mm/CeateaccountActivity.java
@@ -0,0 +1,13 @@
+package com.example.anusaratrokhum.advenger_mm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CeateaccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ceateaccount);
+    }
+}
---
+++ app/src/main/java/com/example/anusaratrokhum/advenger_mm/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.anusaratrokhum.advenger_mm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
