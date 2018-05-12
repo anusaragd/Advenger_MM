67ef5ec49463309be91c228a944307f5f669f41c
AnusaraTrokhum <anusara.orm@gmail.com>
2018-05-12 15:23:42 +0700

    12

--- app/src/main/java/com/example/anusaratrokhum/advenger_mm/Adventure1Activity.java
+++ app/src/main/java/com/example/anusaratrokhum/advenger_mm/Adventure1Activity.java
@@ -2,16 +2,12 @@ package com.example.anusaratrokhum.advenger_mm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-import java.time.Instant;
-
-public class CeateaccountActivity extends AppCompatActivity {
+public class Adventure1Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ceateaccount);
+        setContentView(R.layout.activity_adventure1);
     }
 }
--- app/src/main/java/com/example/anusaratrokhum/advenger_mm/WelcomeActivity.java
+++ app/src/main/java/com/example/anusaratrokhum/advenger_mm/WelcomeActivity.java
@@ -2,16 +2,12 @@ package com.example.anusaratrokhum.advenger_mm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-import java.time.Instant;
-
-public class CeateaccountActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ceateaccount);
+        setContentView(R.layout.activity_welcome);
     }
 }
